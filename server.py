911aa0c306f9ed052d2061a205060470fe8a6432
Christopher Cheape <cheapeone@gmail.com>
2017-04-04 19:49:36 -0400

    Added base template (with the header) and a few pages that use that template.

--- server.py
+++ server.py
@@ -2,18 +2,25 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    return 'Home Page'
+def home():
+    return render_template('home.html')
+
 
 @app.route('/login')
 def login():
-    return 'Log In Page'
+    return render_template('login.html')
 
-@app.route('/style')
-def style():
-    thing = url_for('static', filename='style.css')
-    return thing
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 
 @app.route('/user/')
 @app.route('/user/<name>')
@@ -21,6 +28,5 @@ def user(name=None):
     return render_template('index.html', name=name)
 
 
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
