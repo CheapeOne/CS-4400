727d72c09c47bee7688356c17575bdea5e07dba9
Pradeep <dthineni@gmail.com>
2017-04-05 13:08:59 -0400

    changed the directory schema, run 'python run.py' to run

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from app import app
+from flask import Flask, url_for, render_template
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/user/')
+@app.route('/user/<name>')
+def user(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
