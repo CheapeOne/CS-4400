c4d6ec3d1e5daef5972462bcf5735ced7f12a5e1
Pradeep <dthineni@gmail.com>
2017-04-06 17:30:13 -0400

    views shouldnt have run code for the actual server

--- app/views.py
+++ app/views.py
@@ -21,7 +21,3 @@ def admin():
 @app.route('/user/<name>')
 def user(name=None):
     return render_template('index.html', name=name)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
