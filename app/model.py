48452cdd4c6f3ba03487313a001bc78f3f0c6622
karim <kbadawy3@gmail.com>
2017-04-16 13:48:19 -0400

    a

--- app/model.py
+++ app/model.py
@@ -3,13 +3,20 @@ import pymysql
 
 def check_login(username, password):
       connect()
-      sql='''SELECT username,password from GTChatUsers where username=%s and password = %s'''
-      cursor = self.db.cursor()
+      sql='''SELECT username,password from Users where username=%s and password = %s'''
+      cursor = db.cursor()
       cursor.execute( sql,(self.use1.get(),self.p1.get()))
       db.close()
       if cursor.execute =='':
      
-       messagebox.showerror('Login failed',"Check username and password")
+      messagebox.showerror('Login failed',"Check username and password")
        return
     
     print("CHECKING LOGIN");
+
+def connect():
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='mysql')
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT Host,User FROM user")
