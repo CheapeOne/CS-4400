9ff356153a28590ef5012c813abf0a6ac83c06af
karim <kbadawy3@gmail.com>
2017-04-16 13:52:59 -0400

    a

--- app/model.py
+++ app/model.py
@@ -20,3 +20,38 @@ def connect():
     cur = conn.cursor()
 
     cur.execute("SELECT Host,User FROM user")
+
+def register():
+     self.connect()
+     user = nm4.get()
+     fullname = nm3.get()
+     passwd = ps3.get()
+     confirm = ps4.get()
+     digits = '0123456789'
+     dig  = 0
+     Up = 0
+     upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+     
+     if user == '':
+         messagebox.showerror('Error',"Registration Failed: Empty User name")
+         return
+     if user == '''SELECT username from GTChatUsers where username = user''':
+         messagebox.showerror('Error',"Registration Failed: Username taken")
+         return
+     if passwd != confirm:
+         messagebox.showerror('Error',"Registration Failed: Password does not mach confirmation")
+         return
+     for x in passwd:
+         if x in digits:
+             dig = dig+1
+         elif x in upper:
+             Up = Up+1
+     if dig==0 or Up==0:
+         messagebox.showerror('Error',"Registration Failed: Password needs 1 capital letter and 1 number")
+         return                      
+     toLoginwin()
+     sql ='''INSERT INTO GTChatUsers (username,fullname,password) VALUES (%s,%s,%s)'''
+     cursor2=self.db.cursor()
+     cursor2.execute(sql,(nm4.get(),nm3.get(),ps3.get()))
+     db.commit()
+     db.close()
