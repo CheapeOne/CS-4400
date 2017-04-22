1274c32d6b510037ae2673d1f3d3f0faf87fa87b
Pradeep <dthineni@gmail.com>
2017-04-22 16:51:08 -0400

    merging some changes

--- app/model.py
+++ app/model.py
@@ -3,7 +3,7 @@ import pymysql
 
 def connect():
     db = pymysql.connect(host='localhost', port=3306,
-                           user='root', passwd='kimo64', db='cs4400db')
+                           user='root', passwd='0ktob3r_902107*', db='cs4400db')
 
     cursor = db.cursor()
 
@@ -18,16 +18,16 @@ def disconnect(db, cursor):
 def login_user(username, password):
     db, cursor = connect()
 
-    query = '''SELECT username,password from User where username=%s and password = %s'''
+    query = "SELECT username, password from User where username = {username} and password = {password}"
     cursor = db.cursor()
-    cursor.execute( query,(username,password))
+    cursor.execute(query)
     db.close()
 
     cursor.execute(query)
-    if cursor.execute =='':
-     
-       return('Login failed',"Check username and password")
-       
+    if cursor.execute =="":
+
+       return("Login failed", "Check username and password")
+
 
 
     disconnect(db, cursor)
@@ -35,26 +35,25 @@ def login_user(username, password):
 
 def add_user(emailaddress, user, password, confirm, Type):
     db, cursor = connect()
-    if user == '':
-         return('Error',"Registration Failed: Empty User name")
+    if user == "":
+         return("Error", "Registration Failed: Empty User name")
 
-    if user == '''SELECT username from Users where username = user''':
-         return('Error',"Registration Failed: Username taken")
+    if user == "SELECT username from Users where username = user":
+         return("Error", "Registration Failed: Username taken")
 
-    if emailaddress == '':
-         return('Error',"Registration Failed: Empty Email")
+    if email == "":
+         return("Error", "Registration Failed: Empty Email")
 
-    if emailaddress == '''SELECT username from Users where email = emailaddress''':
-         return('Error',"Registration Failed: Email taken")
-        
-    if password == '':
-         return('Error',"Registration Failed: You must enter a password.")
+    if emailaddress == "SELECT username from Users where email = emailaddress":
+         return("Error", "Registration Failed: Email taken")
+
+    if password == "":
+         return("Error", "Registration Failed: You must enter a password.")
 
     if password != confirm:
-         return('Error',"Registration Failed: Password does not mach confirmation")
+         return("Error", "Registration Failed: Password does not match confirmation")
 
-    query = "INSERT INTO User (Email, Username, Password, User_Type) VALUES ('%(emailaddress)s', '%(user)s', '%(password)s', '%(Type)s')" % locals()
-    
+    query = "INSERT INTO Users (email, username, password, Type) VALUES ({emailaddress}, {user}, {password}, {Type})"
     cursor.execute(query)
     db.commit()
 
@@ -82,12 +81,12 @@ def get_pending_officials():
 def unpend_user(username,status):
     db, cursor = connect()
 
-    query = "SELECT * from User where username = username"
+    query = "SELECT * from User where username = {username}"
 
     cursor.execute(query,(username))
-    if cursor.execute =='':
+    if cursor.execute =="":
         return "Please enter username"
-    sql = "UPDATE City Officials set status = status"
+    sql = "UPDATE City Officials set status = {status}"
     cursor.execute(sql,(status))
     print("Results...")
 
@@ -103,7 +102,7 @@ def add_point(location, timeanddate, Type, Value):
     if type(Value) != int:
         return("Error: Please enter an integer for Data Value.")
 
-    query = "INSERT INTO Data_Point (Data_Type, Data_Value, POI_Location_Name, DateTime) VALUES (Type, Value, location, timeanddate)"
+    query = "INSERT INTO Data_Point (Data_Type, Data_Value, POI_Location_Name, DateTime) VALUES ({Type}, {Value}, {location}, {timeanddate})"
 
     cursor.execute(query)
 
@@ -129,14 +128,14 @@ def get_pending_points():
 def add_location(name, city, state, zip):
     db, cursor = connect()
 
-    query = "INSERT INTO POI (Location_Name, Zip_Code, City, State) VALUES (name, zip, city, state)"
+    query = "INSERT INTO POI (Location_Name, Zip_Code, City, State) VALUES ({name}, {zip}, {city}, {state})"
 
     cursor.execute(query)
 
     disconnect(db, cursor)
 
 
-   
+
 
 def get_locations():
     db, cursor = connect()
@@ -156,11 +155,11 @@ def get_locations():
 def flag_location(name,status):
     db, cursor = connect()
 
-    query = "SELECT * FROM POI WHERE name = Location_Name"
+    query = "SELECT * FROM POI WHERE Location_Name = {name}"
 
     cursor.execute(query,(name))
     if cursor.execute == '':
-        return 'doesnt exist'
+        return "doesnt exist"
     sql = "UPDATE POI set status = status"
     cursor.execute(sql,(status))
     print("Results...")
@@ -175,8 +174,8 @@ def make_report(Type,valueL,valueU,time_dateL,time_dateU):
     db, cursor = connect()
 
     query = "SELECT * FROM Data_Point where Type = Type UNION SELECT * FROM Data_Point WHERE Data_Value BETWEEN valueL AND valueU UNION SELECT * from Data_Point WHERE time_date BETWEEN time_dateL AND time_dateU"
-    
-    
+
+
     cursor.execute(query,(Type,valueL,valueU,time_dateL,time_dateU))
 
     print("Results...")
@@ -185,6 +184,3 @@ def make_report(Type,valueL,valueU,time_dateL,time_dateU):
         print(row)
 
     disconnect(db, cursor)
-
-
-
