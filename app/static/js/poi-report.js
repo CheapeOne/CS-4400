290176665770f77803861921d8d78923195edebf
Christopher Cheape <cheapeone@gmail.com>
2017-04-23 18:15:55 -0400

    Add report view

--- app/static/js/poi-report.js
+++ app/static/js/poi-report.js
@@ -1,5 +1,28 @@
+
+console.log("Getting report...");
 $.get( '/city-official/poi-report/make').done(function (data){
-    console.log(data);
+    data.report.forEach(function(row){
+        addReportRow(row);
+    });
+    console.log("Got report!");
 }).fail(function(res){
     console.log(res.responseText);
 });
+
+function addReportRow(row) {
+    $("#report").append(`
+        <tr>
+            <th>`+ row.Location_Name +`</th>
+            <th>`+ row.City +`</th>
+            <th>`+ row.State +`</th>
+            <th>`+ row.Mold_Min +`</th>
+            <th>`+ row.Mold_Avg +`</th>
+            <th>`+ row.Mold_Max +`</th>
+            <th>`+ row.AQ_Min +`</th>
+            <th>`+ row.AQ_Avg +`</th>
+            <th>`+ row.AQ_Max +`</th>
+            <th>`+ row.Num_Points +`</th>
+            <th>`+ row.Flagged +`</th>
+        </tr>
+    `);
+}
